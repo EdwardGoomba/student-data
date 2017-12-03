321c30e60aff4e0036a650e0027d3d1080d58e38
EdwardGoomba <ask@edwardvdesign.com>
2017-12-03 18:55:11 -0500

    Core files

---
+++ js/students.js
@@ -0,0 +1,40 @@
+var students = [
+  {
+    name: 'Edward',
+    track: 'Full Stack JS',
+    achievements: 12,
+    points: 1219
+  },
+  {
+    name: 'Edward',
+    track: 'Full Stack JS',
+    achievements: 12,
+    points: 1219
+
+  },
+  {
+    name: 'Edward',
+    track: 'Full Stack JS',
+    achievements: 12,
+    points: 1219
+
+  },
+  {
+    name: 'Edward',
+    track: 'Full Stack JS',
+    achievements: 12,
+    points: 1219
+
+  },
+  {
+    name: 'Edward',
+    track: 'Full Stack JS',
+    achievements: 12,
+    points: 1219
+
+  }
+];
+
+for (var key in students) {
+  console.log(students[key]);
+}
